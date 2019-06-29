f0adb8cdd9e152f6add847a2b7e6cabd78d311e6
Md Monsur Hossain <nirob726@gmail.com>
2019-06-30 02:45:54 +0600

    stats_contact_portfolio__cp_nav_section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+$(window).on("load", function() {
+
+	$(".loader .inner").fadeOut(1000, function() {
+		$(".loader").fadeOut(1050);
+	});
+
+	$(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+	});
+
+})
+
+
 $(document).ready(function() {
 
 	$('#slides').superslides({
@@ -8,7 +26,7 @@ $(document).ready(function() {
 
 	var typed = new Typed(".typed", {
 		strings: ["Software Engineer.", "Web Developer.", "Student."],
-		typeSpeed: 110,
+		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
 		showCursor: false
@@ -30,28 +48,117 @@ $(document).ready(function() {
 	        938:{
 	            items:4
 	        }
-
 	    }
 	});
 
 
+	
 
-    var skillsTopOffset = $(".skillsSection").offset().top;
-    console.log(window.pageYOffset);
-    $(window).scroll(function() {
-    	if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-	    		$('.chart').easyPieChart({
-				    easing: 'easeInOut',
-				    barColor: '#fff',
-				    trackColor: false,
-				    scaleColor: false,
-				    lineWidth: 4,
-				    size: 152,
-	         		onStep: function(from, to, percent){
-	         			$(this.el).find('.percent').text(Math.round(percent));
-         }
-    });
-    	}
-    });
+
+	var skillsTopOffset = $(".skillsSection").offset().top;
+	var statsTopOffset = $(".statsSection").offset().top;
+	var countUpFinished = false;
+	$(window).scroll(function() {
+
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+			$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
+
+
+		}
+
+
+		if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+			$(".counter").each(function() {
+				var element = $(this);
+				var endVal = parseInt(element.text());
+
+				element.countup(endVal);
+			})
+
+			countUpFinished = true;
+
+		}
+
+
+	});
+
+
+	$("[data-fancybox]").fancybox();
+
+
+
+	$("#filters a").click(function() {
+
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
+
+		return false;
+	});
+
+
+	$("#navigation li a").click(function(e){
+		e.preventDefault();
+
+		var targetElement = $(this).attr("href");
+		var targetPostion = $(targetElement).offset().top;
+		$("html, body").animate({ scrollTop: targetPostion - 50 }, "slow");
+
+	});
+
+
+	const nav = $('#navigation');
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+		const body = $("body");
+		if($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");
+		}
+		else{
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+		}
+	}
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
